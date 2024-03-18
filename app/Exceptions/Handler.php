488d2c671c9cd85555978c6fbfa0715f7b1cbd75
Zyix09 <ccebol33@gmail.com>
2024-03-18 11:15:20 +0700

    Custom error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -44,5 +46,20 @@ class Handler extends ExceptionHandler
             //
             return response()->json(['message' => 'Unauthenticated or Token Expired'], 429);
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthorizationException) {
+            return response()->view('errors.403', [], 403);
+        }
+        if ($exception instanceof HttpException && $exception->getStatusCode() == 404) {
+            return response()->view('errors.404', [], 404);
+        }
+        if ($exception instanceof HttpException && $exception->getStatusCode() == 500) {
+            return response()->view('errors.500', [], 500);
+        }
+
+        return parent::render($request, $exception);
     }
 }
